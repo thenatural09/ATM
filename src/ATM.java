de2ab3d2ec603e5f57d88829cafa3dbfb36969bb
Troy <tward4@tulane.edu>
2016-09-19 00:07:48 -0400

    An ATM that hardly works with a balance calc and account and password

--- src/ATM.java
+++ src/ATM.java
@@ -7,15 +7,25 @@ import java.util.Scanner;
 public class ATM {
     static Scanner scanner = new Scanner(System.in);
     static boolean keepRunning = true;
+    static HashMap<String, Person> users = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
-            Person person = new Person();
-            person.chooseName();
-        while(keepRunning) {
+        users.put("Alice", new Person("Alice", "helpme"));
+        users.put("Bob", new Person("Bob", "thissucks"));
+
+        Scanner scanner = new Scanner(System.in);
+
+            Person person = new Person("Troy","Baseball");
+            person.enterUsername();
+            person.choosePassword();
+        while (keepRunning) {
             person.chooseOptions();
         }
     }
-
-    HashMap<String,Object> user = new HashMap<>();
-
 }
+
+//      Person person = new Person();
+//        person.chooseName();
+//        while(keepRunning) {
+//        person.chooseOptions();
 
--- src/Person.java
+++ src/Person.java
@@ -1,18 +1,36 @@
+import java.util.HashMap;
+
 /**
  * Created by Troy on 9/14/16.
  */
 public class Person {
     String name;
     String option;
-    int balance = 100;
+    double balance = 100;
+    String password;
 
-    void chooseName() throws Exception {
-        System.out.println("What is your name?");
-        name = ATM.scanner.nextLine();
-        if (name.equalsIgnoreCase("")) {
-            throw new Exception("Please enter your name");
-        }
+    Person(String userName, String newPassword) {
+        setName(userName);
+        setPassword(newPassword);
     }
+    void setName(String userName) {
+        name = userName;
+    }
+
+    void enterUsername() {
+        System.out.println("Enter username:");
+        String name = ATM.scanner.nextLine();
+
+        if (!ATM.users.containsKey(name)) {
+            Person p = new Person(name,"mypassword");
+            ATM.users.put(name, p);
+        }
+
+        Person person = ATM.users.get(name);
+
+        System.out.println("Logged in as: " + person.name);
+    }
+
 
     void chooseOptions() throws Exception {
         System.out.println("1.Check my balance, 2.Withdraw funds, 3.Cancel");
@@ -20,7 +38,7 @@ public class Person {
         if (option.equalsIgnoreCase("withdraw funds")) {
             System.out.println("Enter withdrawal amount");
             String withdrawal = ATM.scanner.nextLine();
-            int withdrawalAmount = Integer.valueOf(withdrawal);
+            double withdrawalAmount = Integer.valueOf(withdrawal);
             if (withdrawalAmount <= balance) {
                 System.out.println("Please take your money");
                 balance = balance - withdrawalAmount;
@@ -35,4 +53,24 @@ public class Person {
             throw new Exception("Thank you and please come again");
         }
     }
+
+    void setBalance(double newBalance) {
+        balance = newBalance;
+    }
+
+    void choosePassword() throws Exception {
+        System.out.println("Please choose a password");
+        String password = ATM.scanner.nextLine();
+        if(password.equalsIgnoreCase("")) {
+            throw new Exception("Please start again and enter valid password");
+        }
+        else {
+            System.out.println("Account created. Select an option:");
+        }
+    }
+    void setPassword(String newPassword) {
+        password = newPassword;
+    }
+
+
 }
